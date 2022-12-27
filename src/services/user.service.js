fc5495c495c66610604f7b89346b1bca3ebd2b7a
baruch yakubov <baruchyaku@gmail.com>
2022-12-27 13:51:10 +0200

    second commit

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,43 @@
+import { storageService } from "./storage.service"
+
 export const userService = {
-    getUser
+    getUser,
+    signUp,
+    addMove
+}
+
+var gUser = {
+    name: "Ochoa Hyde",
+    coins: 100,
+    moves: []
 }
 
 function getUser() {
-    return {
-        name: "Ochoa Hyde",
+    return gUser
+}
+
+// var gUser
+
+// function getUser() {
+//     var user = storageService.load('user')
+//     gUser = user
+//     return gUser
+// }
+
+function signUp(name) {
+    gUser = {
+        name,
         coins: 100,
         moves: []
     }
+}
+
+function addMove(contact, amount) {
+    gUser.moves.unshift({
+        toId: contact._id,
+        to: contact.name,
+        at: Date.now(),
+        amount
+    }
+    )
 }
